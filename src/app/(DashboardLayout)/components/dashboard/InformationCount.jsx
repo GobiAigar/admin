18c566c0a83745573acd1413c9700f9133a5a80a
Adiya-Ochir <corozan1212@gmail.com>
2025-05-27 09:25:28 +0800

    niitlel

---
+++ src/app/(DashboardLayout)/components/dashboard/InformationCount.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+import { useTheme } from "@mui/material/styles";
+import { Grid, Stack, Typography, Avatar, Box } from "@mui/material";
+import { IconArrowUpLeft, IconNews } from "@tabler/icons-react";
+
+import DashboardCard from "@/app/(DashboardLayout)/components/shared/DashboardCard";
+
+const InformationCount = () => {
+  const theme = useTheme();
+  const primary = theme.palette.primary.main;
+  const primarylight = "#ecf2ff";
+
+  const [newsCount, setNewsCount] = useState(0);
+
+  useEffect(() => {
+    const fetchNewsCount = async () => {
+      try {
+        const res = await fetch("https://website-z9b7.onrender.com/api/news");
+        const data = await res.json();
+        setNewsCount(data.length);
+      } catch (error) {
+        console.error("Мэдээ авах үед алдаа гарлаа:", error);
+      }
+    };
+
+    fetchNewsCount();
+  }, []);
+
+  return (
+    <DashboardCard title="Мэдээний тоо">
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Stack direction="row" spacing={1} mt={1} alignItems="center">
+            <Typography variant="subtitle2" color="textSecondary">
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <IconNews width={20} color="#FA896B" />
+                <Typography ml={1}>Нийтлэл</Typography>
+                <Typography variant="h6" fontWeight="700" sx={{ ml: 1 }}>
+                  {newsCount}
+                </Typography>
+              </Box>
+            </Typography>
+          </Stack>
+        </Grid>
+      </Grid>
+    </DashboardCard>
+  );
+};
+
+export default InformationCount;
--- src/app/(DashboardLayout)/page.jsx
+++ src/app/(DashboardLayout)/page.jsx
@@ -1,13 +1,8 @@
-'use client'
-import { Grid, Box } from '@mui/material';
-import PageContainer from '@/app/(DashboardLayout)/components/container/PageContainer';
-// components
-import SalesOverview from '@/app/(DashboardLayout)/components/dashboard/SalesOverview';
-import YearlyBreakup from '@/app/(DashboardLayout)/components/dashboard/YearlyBreakup';
-import RecentTransactions from '@/app/(DashboardLayout)/components/dashboard/RecentTransactions';
-import ProductPerformance from '@/app/(DashboardLayout)/components/dashboard/ProductPerformance';
-import Blog from '@/app/(DashboardLayout)/components/dashboard/Blog';
-import MonthlyEarnings from '@/app/(DashboardLayout)/components/dashboard/MonthlyEarnings';
+"use client";
+import { Grid, Box } from "@mui/material";
+import PageContainer from "@/app/(DashboardLayout)/components/container/PageContainer";
+import SalesOverview from "@/app/(DashboardLayout)/components/dashboard/SalesOverview";
+import InformationCount from "@/app/(DashboardLayout)/components/dashboard/InformationCount";
 
 const Dashboard = () => {
   return (
@@ -17,45 +12,28 @@ const Dashboard = () => {
           <Grid
             size={{
               xs: 12,
-              lg: 8
-            }}>
+              lg: 8,
+            }}
+          >
             <SalesOverview />
           </Grid>
           <Grid
             size={{
               xs: 12,
-              lg: 4
-            }}>
+              lg: 4,
+            }}
+          >
             <Grid container spacing={3}>
               <Grid size={12}>
-                <YearlyBreakup />
+                <InformationCount />
               </Grid>
-              <Grid size={12}>
-                <MonthlyEarnings />
-              </Grid>
+              <Grid size={12}></Grid>
             </Grid>
-          </Grid>
-          <Grid
-            size={{
-              xs: 12,
-              lg: 4
-            }}>
-            <RecentTransactions />
-          </Grid>
-          <Grid
-            size={{
-              xs: 12,
-              lg: 8
-            }}>
-            <ProductPerformance />
-          </Grid>
-          <Grid size={12}>
-            <Blog />
           </Grid>
         </Grid>
       </Box>
     </PageContainer>
   );
-}
+};
 
 export default Dashboard;
