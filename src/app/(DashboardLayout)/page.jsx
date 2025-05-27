8fd06bb65ac1da31e949c3635143ee92fb9b176b
Adiya-Ochir <corozan1212@gmail.com>
2025-05-27 09:38:47 +0800

    little fix

--- src/app/(DashboardLayout)/page.jsx
+++ src/app/(DashboardLayout)/page.jsx
@@ -2,15 +2,7 @@
 import { Grid, Box } from "@mui/material";
 import PageContainer from "@/app/(DashboardLayout)/components/container/PageContainer";
 import SalesOverview from "@/app/(DashboardLayout)/components/dashboard/SalesOverview";
-<<<<<<< HEAD
 import InformationCount from "@/app/(DashboardLayout)/components/dashboard/InformationCount";
-=======
-import YearlyBreakup from "@/app/(DashboardLayout)/components/dashboard/YearlyBreakup";
-import RecentTransactions from "@/app/(DashboardLayout)/components/dashboard/RecentTransactions";
-import ProductPerformance from "@/app/(DashboardLayout)/components/dashboard/ProductPerformance";
-import Blog from "@/app/(DashboardLayout)/components/dashboard/Blog";
-import MonthlyEarnings from "@/app/(DashboardLayout)/components/dashboard/MonthlyEarnings";
->>>>>>> 863375fd9df8d53a81c084c78336a69d299cde2e
 
 const Dashboard = () => {
   return (
@@ -38,28 +30,6 @@ const Dashboard = () => {
               <Grid size={12}></Grid>
             </Grid>
           </Grid>
-<<<<<<< HEAD
-=======
-          <Grid
-            size={{
-              xs: 12,
-              lg: 4,
-            }}
-          >
-            <RecentTransactions />
-          </Grid>
-          <Grid
-            size={{
-              xs: 12,
-              lg: 8,
-            }}
-          >
-            <ProductPerformance />
-          </Grid>
-          <Grid size={12}>
-            <Blog />
-          </Grid>
->>>>>>> 863375fd9df8d53a81c084c78336a69d299cde2e
         </Grid>
       </Box>
     </PageContainer>
